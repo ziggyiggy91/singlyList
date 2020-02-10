13dd64952fb9d4f80f27dcb9f1fd7bacfedd2aef
ismael garcia <up_isms@yahoo.com>
2020-02-09 20:55:25 -0800

    Add Singly Linked List C file

--- linked_list_1.c
+++ linked_list_1.c
@@ -5,11 +5,11 @@
 
 
 struct node{
-	char *data;         // data for node
-	int index;          // index to keep track of node
-	int head;           // head indicator for node
-	struct node *next;  //
-	struct node *prev;
+    char *data;         // data for node
+    int index;          // index to keep track of node
+    int head;           // head indicator for node
+    struct node *next;  //
+    struct node *prev;
 };
 
 /* List prototypes */
@@ -20,8 +20,8 @@ void remove_list(struct node *nodePtr, char *key);
 
 void addHead(struct node *nodePtr, char *data){
     (nodePtr)->head = 1;
-	(nodePtr)->data = data;
- 	(nodePtr)->next = NULL;
+    (nodePtr)->data = data;
+    (nodePtr)->next = NULL;
 }
 
 /*
@@ -31,14 +31,14 @@ void addHead(struct node *nodePtr, char *data){
 */
 
 void replace_data(struct node *nodePtr, char *key, char *newVal){
-	do{
-  		if(nodePtr->data == key){
-		  nodePtr->data = newVal;
-		}
-	  
-		(nodePtr) = (nodePtr)->next;
-	}while(nodePtr != NULL);
-	
+    do{
+        if(nodePtr->data == key){
+          nodePtr->data = newVal;
+        }
+      
+        (nodePtr) = (nodePtr)->next;
+    }while(nodePtr != NULL);
+    
   }
   
 /*
@@ -49,13 +49,13 @@ void replace_data(struct node *nodePtr, char *key, char *newVal){
 
 void remove_list(struct node *nodePtr, char *key){
 
-  while(nodePtr->next != NULL){		 
+  while(nodePtr->next != NULL){      
     if(nodePtr->next->data == key){
-	  printf("replace \n\r");
-	  printf("String: %s\n\r", nodePtr->data);
+      printf("replace \n\r");
+      printf("String: %s\n\r", nodePtr->data);
       nodePtr->next = nodePtr->next->next;
-	}  
-	 (nodePtr) = (nodePtr)->next;
+    }  
+     (nodePtr) = (nodePtr)->next;
    } 
 }
 
@@ -67,22 +67,22 @@ addList:
 */
 
 void addList(struct node *nodePtr, char *data){
-	struct node *newNode = nodePtr;
- 	struct node *tempPtr = nodePtr;
+    struct node *newNode = nodePtr;
+    struct node *tempPtr = nodePtr;
  
     while((newNode)->next != NULL){
- 	  /* populate previous node */
-	  if(newNode->head != 1){
-	    (newNode)->prev = tempPtr;	
-		 tempPtr = tempPtr->next;
-	  }
- 	  (newNode) = (newNode)->next;
-	}
-	
-	(newNode)->next = malloc(sizeof(struct node));	
+      /* populate previous node */
+      if(newNode->head != 1){
+        (newNode)->prev = tempPtr;  
+         tempPtr = tempPtr->next;
+      }
+      (newNode) = (newNode)->next;
+    }
+    
+    (newNode)->next = malloc(sizeof(struct node));  
 
- 	(newNode)->next-> data = data;
-	(newNode)->next-> next = NULL;
+    (newNode)->next-> data = data;
+    (newNode)->next-> next = NULL;
    }
    
 /*
@@ -93,41 +93,41 @@ printList:
 */   
 
 void printList(struct node *nodePtr){
-	struct node *temp = malloc(sizeof(struct node));	
-	temp = nodePtr;
- 	
-	while((temp) != NULL){
-	  printf("List: %s\n\r",(temp)->data);
- 	  (temp) = (temp)->next;
-	}
-	
-	free(temp);
+    struct node *temp = malloc(sizeof(struct node));    
+    temp = nodePtr;
+    
+    while((temp) != NULL){
+      printf("List: %s\n\r",(temp)->data);
+      (temp) = (temp)->next;
+    }
+    
+    free(temp);
 }
 
 int main(){
-	struct node *headPtr = malloc(sizeof(struct node));
-	 
-	printf("------------------------\n\r");
-	addHead(headPtr,"test0");
-	printf("------------------------\n\r");
-	addList(headPtr,"test2");
-	printf("------------------------\n\r");
-	addList(headPtr,"a");
-	addList(headPtr,"b");
-	addList(headPtr,"c");
-	addList(headPtr,"d");
-	addList(headPtr,"e");
-	addList(headPtr,"f");
-	addList(headPtr,"g");
-	printf("------------------------\n\r");
-	printList(headPtr);
-	printf("------------------------\n\r");
+    struct node *headPtr = malloc(sizeof(struct node));
+     
+    printf("------------------------\n\r");
+    addHead(headPtr,"test0");
+    printf("------------------------\n\r");
+    addList(headPtr,"test2");
+    printf("------------------------\n\r");
+    addList(headPtr,"a");
+    addList(headPtr,"b");
+    addList(headPtr,"c");
+    addList(headPtr,"d");
+    addList(headPtr,"e");
+    addList(headPtr,"f");
+    addList(headPtr,"g");
+    printf("------------------------\n\r");
+    printList(headPtr);
+    printf("------------------------\n\r");
     replace_data(headPtr, "c","xxx");
-	printList(headPtr);
-	printf("------------------------\n\r");
+    printList(headPtr);
+    printf("------------------------\n\r");
     remove_list(headPtr,"e");
-	printf("------------------------\n\r");
-	printList(headPtr);
+    printf("------------------------\n\r");
+    printList(headPtr);
 
  
  return 0;
